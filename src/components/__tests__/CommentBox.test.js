4a18254b13afd6ca38c5f1392976b92caf3eb33f
obrummer <obrummer@gmail.com>
2019-05-22 10:44:12 +0300

    initial commit

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import CommentBox from 'components/CommentBox'
+import { mount } from 'enzyme'
+import Root from 'Root'
+
+let wrapped
+
+beforeEach(() => {
+    wrapped = mount(
+    <Root>
+        <CommentBox />
+    </Root>)
+})
+
+afterEach(() => {
+    wrapped.unmount()
+})
+
+it('has a text area and two buttons', () => {
+    expect(wrapped.find('textarea').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(2)
+})
+
+describe('the textarea', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        })
+        wrapped.update()
+    })
+
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    })
+
+    it('input is submitted and text area emptied', () => {
+        wrapped.find('form').simulate('submit')
+        wrapped.update()
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    })
+})
